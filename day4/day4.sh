b2e3cd4eaa9a11cc9d8d684818d661646b3d1367
x123 <x123@users.noreply.github.com>
2023-12-20 09:47:54 +0100

    day4-1 complete

--- day4/day4.sh
+++ day4/day4.sh
@@ -2,4 +2,5 @@
 set -xe
 mix format lib/day4.ex
 mix format mix.exs
-mix escript.build && ./day4 --file ./input/day4-1.example
+mix escript.build && ./day4 --file ./input/day4-1.real
+#mix escript.build && ./day4 --file ./input/day4-1.example
--- day4/lib/day4.ex
+++ day4/lib/day4.ex
@@ -1,3 +1,66 @@
+defmodule Day4.Card do
+  defstruct card_id: nil,
+            winning_numbers: [],
+            player_numbers: [],
+            score: nil
+
+  def parse_game(line) do
+    # IO.puts("> line:#{inspect(line)}")
+    re = ~r/^Card\s+(?<card_id>\d+):\s+(?<winning_numbers>\d.*) \| (?<player_numbers>.*)$/
+    matches = Regex.named_captures(re, line)
+    # IO.puts("> matches:#{inspect(matches)}")
+
+    card_id =
+      Map.get(matches, "card_id")
+      |> String.to_integer()
+
+    winning_numbers =
+      Map.get(matches, "winning_numbers")
+      |> parse_numbers()
+
+    player_numbers =
+      Map.get(matches, "player_numbers")
+      |> parse_numbers()
+
+    # IO.puts(
+    #  "card_id:#{inspect(card_id)} winning_numbers:#{inspect(winning_numbers)} player_numbers:#{inspect(player_numbers)}"
+    # )
+
+    card = %Day4.Card{
+      card_id: card_id,
+      winning_numbers: winning_numbers,
+      player_numbers: player_numbers
+    }
+
+    card = %{
+      card
+      | score: score_card(card)
+    }
+  end
+
+  defp score_card(card) do
+    tmp_list = card.player_numbers -- card.winning_numbers
+    matching = card.player_numbers -- tmp_list
+
+    cond do
+      length(matching) >= 1 ->
+        score = 2 ** (length(matching) - 1)
+        score
+
+      true ->
+        score = 0
+        score
+    end
+  end
+
+  defp parse_numbers(input) do
+    input
+    |> String.split(~r/\s+/, trim: true)
+    |> Enum.map(&String.to_integer(&1))
+    |> List.flatten()
+  end
+end
+
 defmodule Day4 do
   @moduledoc """
   Documentation for `Day4`.
@@ -12,7 +75,17 @@ defmodule Day4 do
       |> parse_args()
       |> load_file()
 
-    debug_print(input)
+    cards =
+      input
+      |> String.split("\n", trim: true)
+      |> Enum.map(&Day4.Card.parse_game(&1))
+
+    sum =
+      cards
+      |> Enum.map(& &1.score)
+      |> Enum.sum()
+
+    IO.puts("Day4-1 Answer:#{inspect(sum)}")
   end
 
   defp load_file({opts, word}) do
